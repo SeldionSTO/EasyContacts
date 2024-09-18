bfbc4eed1cd586f39591eacab5a2daede1003df6
SeldionSTO <seldionsto@gmail.com>
2024-09-19 00:25:19 +0200

    adding sub classes for scalability

--- ui.py
+++ ui.py
@@ -1,15 +1,6 @@
 class ui :
-    def menu(lst) : 
-        for i in range(len(lst)) :
-            print(lst[i])
-        
-        c_input = input("-> ")
-        return c_input
-    
-    def form_person() :
-        name_inpt = input("Enter Name: ")
-        phone_inpt = input("Enter Phone Number: ")
-        email_inpt = input("Enter Email Address: ")
-
-        output_data = {"name":name_inpt, "phone":phone_inpt, "email" : email_inpt}
-        return output_data
+    def __init__(self) -> None:
+        pass
+class menu(ui) :
+    def __init__(self) -> None:
+        super().__init__()
